7b1e2f1e6b0df1e0d6d28524a3d1f6de3890f620
Shankar Malluri <shankarshivam9@gmail.com>
2024-01-17 07:46:28 +0530

    First Commit_Day17A

---
+++ src/test/java/browsers/ChromeBrowserLaunch.java
@@ -0,0 +1,14 @@
+package browsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrowserLaunch {
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+}
